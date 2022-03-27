94b831b9e4f6c75d802215a4243f1c832bc4f972
Lee <lee10202013@gmail.com>
2022-03-27 18:11:54 +0800

    Update clang-format to align linux kernel format

--- gcd.c
+++ gcd.c
@@ -2,26 +2,28 @@
 #include <stdio.h>
 #define MIN(a, b) (((a) < (b)) ? (a) : (b))
 
-uint64_t gcd64(uint64_t u, uint64_t v) {
-  if (!u || !v)
-    return u | v;
-  int u_ctz = __builtin_ctzll(u);
-  int v_ctz = __builtin_ctzll(v);
-  u >>= u_ctz;
-  v >>= v_ctz;
-  do {
-    if (u < v) {
-      v -= u;
-    } else {
-      uint64_t t = u - v;
-      u = v;
-      v = t;
-    }
-  } while (v);
-  return u << MIN(u_ctz, v_ctz);
+uint64_t gcd64(uint64_t u, uint64_t v)
+{
+    if (!u || !v)
+        return u | v;
+    int u_ctz = __builtin_ctzll(u);
+    int v_ctz = __builtin_ctzll(v);
+    u >>= u_ctz;
+    v >>= v_ctz;
+    do {
+        if (u < v) {
+            v -= u;
+        } else {
+            uint64_t t = u - v;
+            u = v;
+            v = t;
+        }
+    } while (v);
+    return u << MIN(u_ctz, v_ctz);
 }
 
-int main() {
-  printf("48, 64, %lld", gcd64(48, 64));
-  printf("38, 133, %lld", gcd64(38, 133));
+int main()
+{
+    printf("48, 64, %lld", gcd64(48, 64));
+    printf("38, 133, %lld", gcd64(38, 133));
 }
