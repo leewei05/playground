c885fa29c835e5abd836b1b1901f3d3e5e3e15a2
leewei05 <lee10202013@gmail.com>
2022-04-02 18:30:28 +0800

    Integrate multiple files into one

--- avg.c
+++ avg.c
@@ -6,6 +6,16 @@ uint32_t average(uint32_t low, uint32_t high)
     return low + (high - low) / 2;
 }
 
+uint32_t average_bitwise(uint32_t a, uint32_t b)
+{
+    return (a >> 1) + (b >> 1) + (a & b & 1);
+}
+
+uint32_t average_bitadder(uint32_t a, uint32_t b)
+{
+    return (a & b) + ((a ^ b) >> 1);
+}
+
 int main()
 {
     int low = 3;
