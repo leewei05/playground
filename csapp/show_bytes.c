add59918bd8c2bd0fbbaebb9148f9db97f858661
leewei05 <lee10202013@gmail.com>
2022-04-04 14:16:21 +0800

    Add side notes

--- csapp/show_bytes.c
+++ csapp/show_bytes.c
@@ -3,6 +3,8 @@
 
 typedef unsigned char *byte_pointer;
 
+// start is the starting address of a consecutive array
+// each array slot is an unit of its type, here is byte_pointer type, which is unsigned char
 void show_bytes(byte_pointer start, size_t len)
 {
     for (size_t i = 0; i < len; i++)
@@ -12,6 +14,9 @@ void show_bytes(byte_pointer start, size_t len)
 
 void show_int(int x)
 {
+    // this tells the compiler to force cast &x to type byte_pointer
+    // it won't change the value of the pointer
+    // the compiler will change the data type of the data that the pointer is pointing to
     show_bytes((byte_pointer) &x, sizeof(int));
 }
 
@@ -22,6 +27,8 @@ void show_float(float x)
 
 void show_pointer(void *x)
 {
+    // void * is a special pointer that doesn't point to any data types
+    // it forces us to cast to any type before we dereference from it
     show_bytes((byte_pointer) &x, sizeof(void *));
 }
 
