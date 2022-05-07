8426e8f9c2a7cdd1a9d800a9f2c3ff6cfa9ecb44
leewei05 <lee10202013@gmail.com>
2022-05-07 15:02:20 +0800

    Create Doxyfile to generate documentation

--- oo/oo.h
+++ oo/oo.h
@@ -1,10 +1,36 @@
-typedef struct object Object;
-typedef int (*func_t)(Object *);
+/**
+ * \file oo.h
+ */
 
+typedef struct object Object;    //!< Object type
+
+/**
+ * Function pointer
+ * @param obj a pointer to Object
+ * @return an integer value.
+ */
+typedef int (*func_t)(Object * obj);
+
+/**
+ * A struct named object
+ */
 struct object {
-  int a, b;
-  func_t add, sub;
+  int a;            //!< int a
+  int b;            //!< int b
+  func_t add, sub;  //!< func
 };
 
-int add(Object *);
-int sub(Object *);
+
+/**
+ * adding member a and member b and returning an integer value.
+ * @param obj is a pointer to Object.
+ * @return The result
+ */
+int add(Object * obj);
+
+/**
+ * subtracting member a and member b and returning an integer value.
+ * @param obj is a pointer to Object.
+ * @return The result
+ */
+int sub(Object * obj);
